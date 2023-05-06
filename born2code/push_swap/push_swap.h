04098a3417ac14edb2ff5e28d33b9ccf05202e7a
jinhyeok <parkjinhyeok87@gmail.com>
2023-05-05 21:54:09 -0700

    type commit- all

---
+++ born2code/push_swap/push_swap.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinhyeok <jinhyeok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 19:58:11 by jinhyeok          #+#    #+#             */
+/*   Updated: 2023/04/28 21:34:28 by jinhyeok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft/libft.h"
+#include <limits.h>
+
+//util.c
+void		error(void);
+int			string_checker(int ac, char **str);
+long long	long_changer(char *str);
+int			is_int(int ac, char **str);
+int			duplicate_checker(int ac, char **str);
+int			isdigit_checker(int ac, char **str);
+
+# endif
